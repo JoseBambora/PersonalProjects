557b9daf41edb93ff4e964802aca73fa7a026397
JoseBambora <zebragapt@gmail.com>
2024-11-29 23:30:57 +0000

    Solved an issue realted with Logging SLF4J and implemented a mechanism to easily include partials on message templates.

--- JDAExtension/src/main/java/org/jdaextension/responses/PreCompileTemplates.java
+++ JDAExtension/src/main/java/org/jdaextension/responses/PreCompileTemplates.java
@@ -2,7 +2,9 @@ package org.jdaextension.responses;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.FileTemplateLoader;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -15,11 +17,14 @@ import java.util.Map;
 public class PreCompileTemplates {
     private Map<String, Template> templates = null;
 
-    private void compileDirectory(String directory, Handlebars handlebars) {
+    private void compileDirectory(String directoryTemplates, String directoryPartials) {
+        Handlebars handlebars = new Handlebars();
         try {
-            List<String> hbsFiles = getFilesWithExtension(directory, ".hbs");
+            List<String> hbsFiles = getFilesWithExtension(directoryTemplates, ".hbs");
             for(String p : hbsFiles) {
-                templates.put(p, handlebars.compile(directory + p));
+                String content = new String(Files.readAllBytes(Paths.get(PreCompileTemplates.class.getClassLoader().getResource(directoryTemplates + p).toURI())));
+                String inline = content.replaceAll("\\{\\{>","\\{\\{>" + directoryPartials);
+                templates.put(p.replaceAll(".hbs",""), handlebars.compileInline(inline));
             }
         } catch (IOException e) {
             System.err.println("Error reading file: \n" + e);
@@ -34,14 +39,12 @@ public class PreCompileTemplates {
                 .filter(Files::isRegularFile)
                 .filter(path -> path.toString().endsWith(extension))
                 .map(path -> resourcePath.relativize(path).toString())
-                .map(name -> name.replaceAll(".hbs",""))
                 .toList();
     }
 
     private PreCompileTemplates() {
         templates = new HashMap<>();
-        Handlebars handlebars = new Handlebars();
-        compileDirectory(System.getenv("VIEWS_FOLDER") + "/",handlebars);
+        compileDirectory(System.getenv("TEMPLATES_FOLDER"), System.getenv("PARTIALS_FOLDER"));
     }
 
     private String getResult(String template, Map<String, Object> variables) {
